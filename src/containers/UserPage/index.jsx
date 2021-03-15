db3c3e16044088cf8835e9a57250b350d96feb6a
pgatic <pgatic@hotmail.com>
2021-03-15 16:11:47 +0100

    add userpage

--- src/containers/UserPage/index.jsx
+++ src/containers/UserPage/index.jsx
@@ -1,9 +1,93 @@
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { setUser } from "./actions";
+import Axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { makeSelectUser } from "./selectors";
+import styled from "styled-components";
+
+const UserContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const UserWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const UserImage = styled.div`
+  width: 7em;
+  height: 7em;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const UserName = styled.h3`
+  font-size: 20px;
+  color: #000;
+  margin: 0;
+`;
+
+const UserEmail = styled.h3`
+  font-size: 18px;
+  color: #353535;
+  margin: 0;
+`;
+
+const stateSelector = createSelector(makeSelectUser, (user) => ({
+  user,
+}));
+
+const actionDispatch = (dispatch) => ({
+  setUser: (user) => dispatch(setUser(user)),
+});
 
 const UserPage = () => {
   const { userId } = useParams();
+  const { user } = useSelector(stateSelector);
+  const { setUser } = actionDispatch(useDispatch());
 
-  return <h2>UserPage - user: {userId}</h2>;
+  const fetchUser = async (id) => {
+    const response = await Axios.get(`https://reqres.in/api/users/${id}`).catch(
+      (err) => {
+        console.log("Err: ", err);
+      }
+    );
+
+    if (response) {
+      setUser(response.data.data);
+    }
+  };
+
+  useEffect(() => {
+    if (userId && userId !== "") {
+      fetchUser(userId);
+    }
+  }, [userId]);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <UserContainer>
+      <UserWrapper>
+        <UserImage>
+          <img src={user.avatar} />
+        </UserImage>
+        <UserName>
+          {user.first_name} {user.last_name}
+        </UserName>
+        <UserEmail>{user.email}</UserEmail>
+      </UserWrapper>
+    </UserContainer>
+  );
 };
 
 export default UserPage;
--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 import { createStore, combineReducers } from "redux";
 // import reducers
 import homePage from "./containers/HomePage/reducer";
+import userPage from "./containers/UserPage/reducer";
 
 // combine reducers
-const reducers = combineReducers({ homePage });
+const reducers = combineReducers({ homePage, userPage });
 
 // create and export store
 export default createStore(reducers);
